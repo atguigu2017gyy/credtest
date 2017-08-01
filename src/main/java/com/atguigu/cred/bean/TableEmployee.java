c8c233b827ed491ef2e2bac07cb6df7a1e144804
atguigu2017gyy <atguigu2017gyy@126.com>
2017-08-01 20:37:25 +0800

    第六次

--- src/main/java/com/atguigu/cred/bean/TableEmployee.java
+++ src/main/java/com/atguigu/cred/bean/TableEmployee.java
@@ -33,7 +33,7 @@ public class TableEmployee {
 
     @Override
     public String toString() {
-        return "TableEmployee [dfdereree=" + empId + ", empName=" + empName + ", salary=" + salary + "]";
+        return "TableEmployee [dfderereeDDDDDD=" + empId + ", empName=" + empName + ", salary=" + salary + "]";
     }
     
 }
